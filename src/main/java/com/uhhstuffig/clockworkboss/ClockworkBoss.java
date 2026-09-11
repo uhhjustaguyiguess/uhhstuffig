6d3f5d90f623b6b1f1f25d5329b0b63b5ba57092
uhhjustaguyiguess <iranoutofnamesplshelp@gmail.com>
2026-09-11 23:06:52 +0000

    Boss setup

--- src/main/java/com/uhhstuffig/clockworkboss/ClockworkBoss.java
+++ src/main/java/com/uhhstuffig/clockworkboss/ClockworkBoss.java
@@ -1,7 +1,8 @@
 package com.uhhstuffig.clockworkboss;
 
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ClockworkBoss.MODID)
 public class ClockworkBoss {
@@ -9,5 +10,16 @@ public class ClockworkBoss {
     public static final String MODID = "clockworkboss";
 
     public ClockworkBoss() {
+        var modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModEntities.ENTITY_TYPES.register(modEventBus);
+        modEventBus.addListener(this::registerAttributes);
+    }
+
+    private void registerAttributes(EntityAttributeCreationEvent event) {
+        event.put(
+                ModEntities.CLOCKWORK_BEHEMOTH.get(),
+                ClockworkBehemothEntity.createAttributes().build()
+        );
     }
 }
