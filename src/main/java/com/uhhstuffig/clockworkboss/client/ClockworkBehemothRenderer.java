e290ef2e3597e62c66cea4bdafc62d6428103588
uhhjustaguyiguess <iranoutofnamesplshelp@gmail.com>
2026-09-14 23:21:29 +0300

    Update ClockworkBehemothRenderer.java
    why is it so goddamn hard

--- src/main/java/com/uhhstuffig/clockworkboss/client/ClockworkBehemothRenderer.java
+++ src/main/java/com/uhhstuffig/clockworkboss/client/ClockworkBehemothRenderer.java
@@ -1,1 +1,12 @@
-placeholder
+package com.uhhstuffig.clockworkboss.client;
+
+import com.uhhstuffig.clockworkboss.ClockworkBehemothEntity;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import software.bernie.geckolib.renderer.GeoEntityRenderer;
+
+public class ClockworkBehemothRenderer extends GeoEntityRenderer<ClockworkBehemothEntity> {
+
+    public ClockworkBehemothRenderer(EntityRendererProvider.Context renderManager) {
+        super(renderManager, new ClockworkBehemothModel());
+    }
+}
